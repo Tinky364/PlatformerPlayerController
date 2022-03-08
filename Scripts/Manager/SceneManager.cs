bff0316beee9e1cab955271fdacf03e1d01fed25
Halit Kesim <halitkesim@gmail.com>
2022-03-09 01:13:18 +0300

    Pixel perfect camera

--- Scripts/Manager/SceneManager.cs
+++ Scripts/Manager/SceneManager.cs
@@ -4,13 +4,17 @@ namespace Manager
 {
     public class SceneManager : Node
     {
-        public CanvasLayer World { get; private set; }
+        public ViewportContainer World { get; private set; }
         public CanvasLayer Gui { get; private set; }
 
         public override void _EnterTree()
         {
-            World = GetNode<CanvasLayer>("World");
+            World = GetNode<ViewportContainer>("World");
             Gui = GetNode<CanvasLayer>("Gui");
+
+            World.RectScale = new Vector2(5, 5);
+            World.RectPosition = new Vector2(-5, -5);
+            Gui.Scale = Vector2.One;
 
             PauseMode = PauseModeEnum.Process;
             World.PauseMode = PauseModeEnum.Stop;
--- Scripts/Other/CamController.cs
+++ Scripts/Other/CamController.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Manager;
 using PlayerStateMachine;
 
 namespace Other
@@ -6,41 +7,59 @@ namespace Other
     public class CamController : Camera2D
     {
         [Export]
+        private SmoothType _curSmoothType;
+        [Export]
         private NodePath _playerPath = default;
         [Export(PropertyHint.Range, "0")]
         private float _offsetAcceleration = 2f;
         [Export]
-        private bool _smoothing;
-        [Export]
-        private bool _pixelSmoothing;
-        [Export]
-        private float _smoothingDur = 0.15f;
+        private float _smoothingDur = 0.2f;
         
+        private enum SmoothType { Pixel, Normal, None }
         private Player _player;
-        private Vector2 _destPos;
+        private Vector2 _destinationPos;
         private Vector2 _curPos;
         
         public override void _Ready()
         {
+            SmoothingEnabled = _curSmoothType == SmoothType.Normal;
+
             _player = GetNode<Player>(_playerPath);
-            _curPos = _player.GlobalPosition;
-            if (_smoothing && !_pixelSmoothing) SmoothingEnabled = true;
-            else SmoothingEnabled = false;
         }
 
         public override void _PhysicsProcess(float delta)
         {
-            if (_pixelSmoothing) PixelPerfectSmoothing(delta);
-            else GlobalPosition = _player.GlobalPosition;
-            //SetCamOffset(delta);
+            switch (_curSmoothType)
+            {
+                case SmoothType.Pixel: 
+                    PixelSmoothing(delta);
+                    break;
+                case SmoothType.Normal:
+                    GlobalPosition = _player.GlobalPosition;
+                    break;
+                case SmoothType.None:
+                    GlobalPosition = _player.GlobalPosition;
+                    break;
+            }
+            SetCamOffset(delta);
             ForceUpdateScroll();
         }
 
-        private void PixelPerfectSmoothing(float delta)
+        //TODO arrange limits
+        private void PixelSmoothing(float delta)
         {
-            _destPos = _player.GlobalPosition;
-            _curPos += new Vector2(
-                _destPos.x - _curPos.x, _destPos.y - _curPos.y) * (1 / _smoothingDur * delta);
+            _destinationPos = _player.GlobalPosition;
+            if (_destinationPos.x - 160 > LimitLeft && _destinationPos.x + 160 < LimitRight)
+            {
+                _curPos.x += (_destinationPos.x - _curPos.x) * (1 / _smoothingDur * delta);
+            }
+            if (_destinationPos.y - 90 > LimitTop && _destinationPos.y + 90 < LimitBottom)
+            {
+                _curPos.y += (_destinationPos.y - _curPos.y) * (1 / _smoothingDur * delta);
+            }
+            var subPixel = _curPos.Round() - _curPos;
+            if (GM.S.CurrentScene.World.Material is ShaderMaterial shader)
+                shader.SetShaderParam("cam_offset", subPixel.Clamped(2f));
             GlobalPosition = _curPos.Round();
         }
 
